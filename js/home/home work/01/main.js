843111bc33c084379d4139171807911908a142b7
Александр Крушинский < >
2017-04-19 11:11:34 +0300

    Initial commit

---
+++ js/home/home work/01/main.js
@@ -0,0 +1,13 @@
+// 
+//
+//var color = prompt("what color you can go");
+//var r = "red";
+//var g = "green";
+//var y = "yellow";
+//if(color == r){
+//    console.log("stop")
+//}else if(color==y){
+//    console.log("wait")
+//}else if(color == g){
+//    console.log("go")
+//}
---
+++ js/home/sasha individual/01/main.js
@@ -0,0 +1,63 @@
+// alert("hello");
+console.log("hello");
+var a = 'new hello';
+console.log(a);
+var b = 100.55;
+console.log(b);
+document.write(a);
+document.write(b);
+
+console.log('100' + '200');
+console.log(100 + 200 + "asd");
+console.log("" + 100 + 200);
+
+console.log(typeof (typeof  "aaa"));
+console.log(typeof "hello");
+console.log(typeof 123.123);
+console.log(typeof true);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof NaN);
+console.log("d"/"a");
+var c ;
+console.log(typeof c);
+//  стакакн - кола
+var bool = true;
+//         кола
+console.log(bool);
+bool = false;
+console.log(bool);
+// > < >= <= == === != !==
+bool = 5 == 6;
+console.log(bool);
+console.log(100 == '100');
+console.log(100 !== '100');
+// var name = value;
+
+// var fromPrompt = prompt("color?");
+// if(fromPrompt == 'red'){
+//     console.log("wait!!");
+// }else{
+//     console.log("go!");
+// }
+
+var wallet = 2000;
+var price = prompt("price?");
+// if(wallet >= price){
+//     console.log("you can buy it");
+// }else{
+//     console.log("go away!");
+// }
+if(wallet >= price){
+    console.log("you can buy it");
+}else if (wallet+500 >= price){
+    console.log("you can buy it but you got credit");
+}else if(wallet+1000 >= price) {
+    console.log("you can buy it but you got bigger credit");
+}else{
+    console.log("go away!");
+}
+
+// kruushik89@gmail.com;
+
+
